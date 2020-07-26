482a37109205e4d517f46301f255354e49adf927
avishuk <avirshuk@gmail.com>
2020-07-27 02:21:05 +0530

    assignment 5

--- site/mod5_sol/assignment5-solution-starter/js/script.js
+++ site/mod5_sol/assignment5-solution-starter/js/script.js
@@ -102,7 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      console.log(chosenCategoryShortName);
+      // console.log(chosenCategoryShortName);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -122,7 +122,7 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-      innerHTML("#main-content",homeHtmlToInsertIntoMainPage);
+      insertHTML("#main-content",homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
